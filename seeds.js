d10c84fe3d415b4e4442af8117e3cb54d9fe3eef
Sean <stillot2@gmail.com>
2018-07-31 06:50:47 +0000

    re-seed deploy

--- seeds.js
+++ seeds.js
@@ -5,24 +5,29 @@ var User = require("./models/user");
 
 // actually a teardown of db
 function seedDB(){
-   Campground.remove({}, function(err){
-        if(err){
-            console.log(err);
-        }
-        console.log("removed campgrounds!");
-        Comment.remove({}, function(err) {
-            if(err){
-                console.log(err);
-            }
-            console.log("removed comments!");
-                User.remove({}, function(err) {
-                    if(err){
-                        console.log(err);
-                    }
-                    console.log("removed users!");
-            });
+    Campground.find({}, function(err, sites){
+        sites.forEach(function(x){
+            x.delete();
         });
-    }); 
+    });
+//   Campground.remove({}, function(err){
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log("removed campgrounds!");
+//         Comment.remove({}, function(err) {
+//             if(err){
+//                 console.log(err);
+//             }
+//             console.log("removed comments!");
+//                 User.remove({}, function(err) {
+//                     if(err){
+//                         console.log(err);
+//                     }
+//                     console.log("removed users!");
+//             });
+//         });
+//     }); 
 }
  
 module.exports = seedDB;
